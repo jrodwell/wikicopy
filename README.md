29ccaf0d119f56ef256376faf12a176413da2506
John Rodwell <john.rodwell@draysontechnologies.com>
2017-09-12 13:42:48 +0100

    First Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Wikicopy Project Development Environment Installation Instructions
+
+1) Install WordPress latest version in your build directory
+2) Clone the git repository https://github.com/jrodwell/wikicopy.git to the same direcry
+
---
+++ wp-content/themes/wikicopy/page.php
@@ -0,0 +1,15 @@
+<?php get_header(); ?>
+
+	<main role="main">
+		<!-- section -->
+		<section>
+
+			<h1><?php the_title(); ?></h1>
+
+			This is the page template<br /><br />
+
+		</section>
+		<!-- /section -->
+	</main>
+
+<?php get_footer(); ?>
